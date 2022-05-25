c01b2ace183d58ebc6783ee0250978d4d3672d3f
RFNoC <rj.geng@gmail.com>
2022-05-25 10:49:36 -0700

    Alert: Closable

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
-import { DatePicker, message } from 'antd';
+import { DatePicker, message, Alert } from 'antd';
 import 'antd/dist/antd.css';
 import './index.css';
+
+const onClose = e => {
+  console.log(e, 'I was closed.');
+};
 
 const App = () => {
   const [date, setDate] = useState(null);
@@ -16,7 +20,24 @@ const App = () => {
     <div style={{ width: 400, margin: '100px auto' }}>
       <DatePicker onChange={handleChange} />
       <div style={{ marginTop: 16 }}>
-        Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
+        <Alert
+          message="Selected Date"
+          description={date ? date.format('YYYY-MM-DD') : 'None'}
+        />
+        <Alert message="Success Text" type="success" />
+        <Alert
+          message="Warning Text Warning Text Warning TextW arning Text Warning Text Warning TextWarning Text"
+          type="warning"
+          closable
+          onClose={onClose}
+        />
+        <Alert
+          message="Error Text"
+          description="Error Description Error Description Error Description Error Description Error Description Error Description"
+          type="error"
+          closable
+          onClose={onClose}
+        />
       </div>
     </div>
   );
