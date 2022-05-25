ad20e8f36eb6d98440776b9a9c5688a715ab3a4e
RFNoC <rj.geng@gmail.com>
2022-05-25 11:23:02 -0700

    Alert: Banner

--- src/index.js
+++ src/index.js
@@ -24,7 +24,18 @@ const App = () => {
           message="Selected Date"
           description={date ? date.format('YYYY-MM-DD') : 'None'}
         />
-        <Alert message="Info Text" type="info" closeText="Close Now" />
+        <br />
+        <Alert message="Warning text" banner />
+        <br />
+        <Alert
+          message="Very long warning text warning text text text text text text text"
+          banner
+          closable
+        />
+        <br />
+        <Alert showIcon={false} message="Warning text without icon" banner />
+        <br />
+        <Alert type="error" message="Error text" banner />
       </div>
     </div>
   );
