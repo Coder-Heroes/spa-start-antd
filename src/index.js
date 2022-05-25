cc19a795989af6c2654376015378aeec9f01f0aa
RFNoC <rj.geng@gmail.com>
2022-05-25 10:02:46 -0700

    Use and modify an antd component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Button, DatePicker, version } from 'antd';
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { DatePicker, message } from 'antd';
 import 'antd/dist/antd.css';
 import './index.css';
 
-ReactDOM.render(
-  <div className="App">
-    <h1>antd version: {version}</h1>
-    <DatePicker />
-    <Button type="primary" style={{ marginLeft: 8 }}>
-      Primary Button
-    </Button>
-  </div>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [date, setDate] = useState(null);
+  const handleChange = value => {
+    message.info(
+      `Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`
+    );
+    setDate(value);
+  };
+  return (
+    <div style={{ width: 400, margin: '100px auto' }}>
+      <DatePicker onChange={handleChange} />
+      <div style={{ marginTop: 16 }}>
+        Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
+      </div>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('root'));
