663c5a5fccbc94d30c64256babd722ae0bfa3e42
RFNoC <rj.geng@gmail.com>
2022-05-25 13:28:49 -0700

    Alert: ErrorBoundary

--- src/index.js
+++ src/index.js
@@ -1,56 +1,38 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
-import { DatePicker, message, Alert } from 'antd';
+import { DatePicker, message, Button, Alert } from 'antd';
+
 import Marquee from 'react-fast-marquee';
 import { TextLoop } from 'react-text-loop-next';
 import 'antd/dist/antd.css';
 import './index.css';
 
-const onClose = e => {
-  console.log(e, 'I was closed.');
-};
+const { ErrorBoundary } = Alert;
 
-const App = () => {
-  const [visible_1, setVisible_1] = useState(true);
-  const handleClose_1 = () => {
-    setVisible_1(false);
+const ThrowError = () => {
+  const [error, setError] = useState();
+
+  const onClick = () => {
+    setError(new Error('An Uncaught Error'));
   };
-  const [visible_2, setVisible_2] = useState(true);
-  const handleClose_2 = () => {
-    setVisible_2(false);
-  };
-  const [date, setDate] = useState(null);
-  const handleChange = value => {
-    message.info(
-      `Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`
-    );
-    setDate(value);
-  };
+
+  if (error) {
+    throw error;
+  }
+
   return (
-    <div style={{ width: 400, margin: '100px auto' }}>
-      <DatePicker onChange={handleChange} />
-      <div style={{ marginTop: 16 }}>
-        {visible_1 || visible_2 ? (
-          <>
-            <Alert
-              message="Selected Date"
-              description={date ? date.format('YYYY-MM-DD') : 'None'}
-              type="success"
-              closable
-              afterClose={handleClose_1}
-            />
-            <Alert
-              message="Alert Message Text"
-              type="success"
-              closable
-              afterClose={handleClose_2}
-            />
-          </>
-        ) : null}
-        <p>placeholder text here</p>
-      </div>
-    </div>
+    <Button danger onClick={onClick}>
+      Click me to throw a error
+    </Button>
   );
 };
+
+const App = () => (
+  <ErrorBoundary>
+    <ThrowError />
+  </ErrorBoundary>
+);
+
+export default App;
 
 render(<App />, document.getElementById('root'));
