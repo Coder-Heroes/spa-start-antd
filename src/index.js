f320995f91b7bf82232d250a9d5914573fd31ec9
RFNoC <rj.geng@gmail.com>
2022-05-25 11:11:34 -0700

    Alert: icon

--- src/index.js
+++ src/index.js
@@ -24,25 +24,34 @@ const App = () => {
           message="Selected Date"
           description={date ? date.format('YYYY-MM-DD') : 'None'}
         />
+        <Alert message="Success Tips" type="success" showIcon />
+        <Alert message="Informational Notes" type="info" showIcon />
+        <Alert message="Warning" type="warning" showIcon closable />
+        <Alert message="Error" type="error" showIcon />
         <Alert
-          message="Success Text"
-          description="Success Description Success Description Success Description"
+          message="Success Tips"
+          description="Detailed description and advice about successful copywriting."
           type="success"
+          showIcon
         />
         <Alert
-          message="Info Text"
-          description="Info Description Info Description Info Description Info Description"
+          message="Informational Notes"
+          description="Additional description and information about copywriting."
           type="info"
+          showIcon
         />
         <Alert
-          message="Warning Text"
-          description="Warning Description Warning Description Warning Description Warning Description"
+          message="Warning"
+          description="This is a warning notice about copywriting."
           type="warning"
+          showIcon
+          closable
         />
         <Alert
-          message="Error Text"
-          description="Error Description Error Description Error Description Error Description"
+          message="Error"
+          description="This is an error message about copywriting."
           type="error"
+          showIcon
         />
       </div>
     </div>
