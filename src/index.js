b21e45d558d51c24c6d01011ff2020f2940ed895
RFNoC <rj.geng@gmail.com>
2022-05-25 11:08:14 -0700

    Alert: Description

--- src/index.js
+++ src/index.js
@@ -24,10 +24,26 @@ const App = () => {
           message="Selected Date"
           description={date ? date.format('YYYY-MM-DD') : 'None'}
         />
-        <Alert message="Success Text" type="success" />
-        <Alert message="Info Text" type="info" />
-        <Alert message="Warning Text" type="warning" />
-        <Alert message="Error Text" type="error" />
+        <Alert
+          message="Success Text"
+          description="Success Description Success Description Success Description"
+          type="success"
+        />
+        <Alert
+          message="Info Text"
+          description="Info Description Info Description Info Description Info Description"
+          type="info"
+        />
+        <Alert
+          message="Warning Text"
+          description="Warning Description Warning Description Warning Description Warning Description"
+          type="warning"
+        />
+        <Alert
+          message="Error Text"
+          description="Error Description Error Description Error Description Error Description"
+          type="error"
+        />
       </div>
     </div>
   );
