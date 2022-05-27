99f2746223132f29511c4e80df1f4b054a557dab
RFNoC <rj.geng@gmail.com>
2022-05-27 16:55:35 -0700

    Button: Type

--- src/index.js
+++ src/index.js
@@ -5,49 +5,16 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
 
-import { Divider } from 'antd';
+import { Button } from 'antd';
 
 const App = () => (
   <>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
-      merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen,
-      quo modo.
-    </p>
-    <Divider>Text</Divider>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
-      merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen,
-      quo modo.
-    </p>
-    <Divider orientation="left">Left Text</Divider>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
-      merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen,
-      quo modo.
-    </p>
-    <Divider orientation="right">Right Text</Divider>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
-      merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen,
-      quo modo.
-    </p>
-    <Divider orientation="left" orientationMargin="0">
-      Left Text with 0 orientationMargin
-    </Divider>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
-      merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen,
-      quo modo.
-    </p>
-    <Divider orientation="right" orientationMargin={50}>
-      Right Text with 50px orientationMargin
-    </Divider>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
-      merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen,
-      quo modo.
-    </p>
+    <Button type="primary">Primary Button</Button>
+    <Button>Default Button</Button>
+    <Button type="dashed">Dashed Button</Button>
+    <br />
+    <Button type="text">Text Button</Button>
+    <Button type="link">Link Button</Button>
   </>
 );
 
