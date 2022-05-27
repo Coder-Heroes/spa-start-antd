299a89b453657846199fddaa1e671df2b94885d4
RFNoC <rj.geng@gmail.com>
2022-05-27 07:17:44 -0700

    Grid card

--- src/index.js
+++ src/index.js
@@ -4,60 +4,24 @@ import { render } from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
 
-import {
-  EditOutlined,
-  EllipsisOutlined,
-  SettingOutlined,
-} from '@ant-design/icons';
-import { Avatar, Card, Skeleton, Switch } from 'antd';
-const { Meta } = Card;
-
-const App = () => {
-  const [loading, setLoading] = useState(true);
-
-  const onChange = checked => {
-    setLoading(!checked);
-  };
-
-  return (
-    <>
-      <Switch checked={!loading} onChange={onChange} />
-
-      <Card
-        style={{
-          width: 300,
-          marginTop: 16,
-        }}
-        loading={loading}
-      >
-        <Meta
-          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-          title="Card title"
-          description="This is the description"
-        />
-      </Card>
-
-      <Card
-        style={{
-          width: 300,
-          marginTop: 16,
-        }}
-        actions={[
-          <SettingOutlined key="setting" />,
-          <EditOutlined key="edit" />,
-          <EllipsisOutlined key="ellipsis" />,
-        ]}
-      >
-        <Skeleton loading={loading} avatar active>
-          <Meta
-            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-            title="Card title"
-            description="This is the description"
-          />
-        </Skeleton>
-      </Card>
-    </>
-  );
+import { Card } from 'antd';
+const gridStyle = {
+  width: '25%',
+  textAlign: 'center',
 };
+
+const App = () => (
+  <Card title="Card Title">
+    <Card.Grid style={gridStyle}>Content</Card.Grid>
+    <Card.Grid hoverable={false} style={gridStyle}>
+      Content
+    </Card.Grid>
+    <Card.Grid style={gridStyle}>Content</Card.Grid>
+    <Card.Grid style={gridStyle}>Content</Card.Grid>
+    <Card.Grid style={gridStyle}>Content</Card.Grid>
+    <Card.Grid style={gridStyle}>Content</Card.Grid>
+    <Card.Grid style={gridStyle}>Content</Card.Grid>
+  </Card>
+);
 
 render(<App />, document.getElementById('root'));
