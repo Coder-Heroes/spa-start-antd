49e30f5dd84b7beadc403b0a4135dc691c2c14c7
RFNoC <rj.geng@gmail.com>
2022-05-26 23:36:59 -0700

    Card in column

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
-import { Card } from 'antd';
+
 import 'antd/dist/antd.css';
 import './index.css';
 
-const { Meta } = Card;
+import { Card, Col, Row } from 'antd';
 
 const App = () => (
-  <Card
-    hoverable
-    style={{
-      width: 240,
-    }}
-    cover={
-      <img
-        alt="example"
-        src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-      />
-    }
-  >
-    <Meta title="Europe Street beat" description="www.instagram.com" />
-  </Card>
+  <div className="site-card-wrapper">
+    <Row gutter={16}>
+      <Col span={8}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+    </Row>
+  </div>
 );
 
 render(<App />, document.getElementById('root'));
