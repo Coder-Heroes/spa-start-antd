3881b43464623aecdbbecde180d393cd9b14eb62
RFNoC <rj.geng@gmail.com>
2022-05-27 15:14:28 -0700

    Layout: Header Sider 2

--- src/index.js
+++ src/index.js
@@ -4,49 +4,86 @@ import { render } from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
 
+import {
+  LaptopOutlined,
+  NotificationOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
 import { Breadcrumb, Layout, Menu } from 'antd';
-const { Header, Content, Footer } = Layout;
+
+const { Header, Content, Sider } = Layout;
+const items1 = ['1', '2', '3'].map(key => ({
+  key,
+  label: `nav ${key}`,
+}));
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1);
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `subnav ${key}`,
+      children: new Array(4).fill(null).map((_, j) => {
+        const subKey = index * 4 + j + 1;
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        };
+      }),
+    };
+  }
+);
 
 const App = () => (
-  <Layout className="layout">
-    <Header>
+  <Layout>
+    <Header className="header">
       <div className="logo" />
       <Menu
         theme="dark"
         mode="horizontal"
         defaultSelectedKeys={['2']}
-        items={new Array(15).fill(null).map((_, index) => {
-          const key = index + 1;
-          return {
-            key,
-            label: `nav ${key}`,
-          };
-        })}
+        items={items1}
       />
     </Header>
-    <Content
-      style={{
-        padding: '0 50px',
-      }}
-    >
-      <Breadcrumb
+    <Layout>
+      <Sider width={200} className="site-layout-background">
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{
+            height: '100%',
+            borderRight: 0,
+          }}
+          items={items2}
+        />
+      </Sider>
+      <Layout
         style={{
-          margin: '16px 0',
+          padding: '0 24px 24px',
         }}
       >
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-      <div className="site-layout-content">Content</div>
-    </Content>
-    <Footer
-      style={{
-        textAlign: 'center',
-      }}
-    >
-      Ant Design ©2018 Created by Ant UED
-    </Footer>
+        <Breadcrumb
+          style={{
+            margin: '16px 0',
+          }}
+        >
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Content
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
   </Layout>
 );
 
