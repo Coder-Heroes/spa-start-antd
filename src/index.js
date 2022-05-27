12b2f33f14e3259b33262fd065f1f00536b05a73
RFNoC <rj.geng@gmail.com>
2022-05-26 23:44:46 -0700

    Loading card

--- src/index.js
+++ src/index.js
@@ -4,28 +4,60 @@ import { render } from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
 
-import { Card, Col, Row } from 'antd';
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
+import { Avatar, Card, Skeleton, Switch } from 'antd';
+const { Meta } = Card;
 
-const App = () => (
-  <div className="site-card-wrapper">
-    <Row gutter={16}>
-      <Col span={8}>
-        <Card title="Card title" bordered={false}>
-          Card content
-        </Card>
-      </Col>
-      <Col span={8}>
-        <Card title="Card title" bordered={false}>
-          Card content
-        </Card>
-      </Col>
-      <Col span={8}>
-        <Card title="Card title" bordered={false}>
-          Card content
-        </Card>
-      </Col>
-    </Row>
-  </div>
-);
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  const onChange = checked => {
+    setLoading(!checked);
+  };
+
+  return (
+    <>
+      <Switch checked={!loading} onChange={onChange} />
+
+      <Card
+        style={{
+          width: 300,
+          marginTop: 16,
+        }}
+        loading={loading}
+      >
+        <Meta
+          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+          title="Card title"
+          description="This is the description"
+        />
+      </Card>
+
+      <Card
+        style={{
+          width: 300,
+          marginTop: 16,
+        }}
+        actions={[
+          <SettingOutlined key="setting" />,
+          <EditOutlined key="edit" />,
+          <EllipsisOutlined key="ellipsis" />,
+        ]}
+      >
+        <Skeleton loading={loading} avatar active>
+          <Meta
+            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+            title="Card title"
+            description="This is the description"
+          />
+        </Skeleton>
+      </Card>
+    </>
+  );
+};
 
 render(<App />, document.getElementById('root'));
