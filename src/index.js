e83a2baef57d5b1a93fe1b8f5a78c956e23b753d
RFNoC <rj.geng@gmail.com>
2022-05-27 16:46:55 -0700

    Divider: Divider with title

--- src/index.js
+++ src/index.js
@@ -14,22 +14,34 @@ const App = () => (
       merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen,
       quo modo.
     </p>
-    <Divider plain>Text</Divider>
+    <Divider>Text</Divider>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
       merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen,
       quo modo.
     </p>
-    <Divider orientation="left" plain>
-      Left Text
+    <Divider orientation="left">Left Text</Divider>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
+      merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen,
+      quo modo.
+    </p>
+    <Divider orientation="right">Right Text</Divider>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
+      merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen,
+      quo modo.
+    </p>
+    <Divider orientation="left" orientationMargin="0">
+      Left Text with 0 orientationMargin
     </Divider>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
       merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen,
       quo modo.
     </p>
-    <Divider orientation="right" plain>
-      Right Text
+    <Divider orientation="right" orientationMargin={50}>
+      Right Text with 50px orientationMargin
     </Divider>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
