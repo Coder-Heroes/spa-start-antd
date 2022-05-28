3dfd705e260977dde61153be65d7364feafb0826
RFNoC <rj.geng@gmail.com>
2022-05-27 18:43:01 -0700

    Icons: Two-tone icon and colorful icon

--- src/index.js
+++ src/index.js
@@ -5,21 +5,18 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
 
-import { createFromIconfontCN } from '@ant-design/icons';
+import {
+  CheckCircleTwoTone,
+  HeartTwoTone,
+  SmileTwoTone,
+} from '@ant-design/icons';
 import { Space } from 'antd';
-const IconFont = createFromIconfontCN({
-  scriptUrl: [
-    '//at.alicdn.com/t/font_1788044_0dwu4guekcwr.js', // icon-javascript, icon-java, icon-shoppingcart (overrided)
-    '//at.alicdn.com/t/font_1788592_a5xf2bdic3u.js', // icon-shoppingcart, icon-python
-  ],
-});
 
 const App = () => (
   <Space>
-    <IconFont type="icon-javascript" />
-    <IconFont type="icon-java" />
-    <IconFont type="icon-shoppingcart" />
-    <IconFont type="icon-python" />
+    <SmileTwoTone />
+    <HeartTwoTone twoToneColor="#eb2f96" />
+    <CheckCircleTwoTone twoToneColor="#52c41a" />
   </Space>
 );
 
