30991ded429d6535a4cc7395eaee025e85983caf
RFNoC <rj.geng@gmail.com>
2022-05-27 20:48:27 -0700

    Grid: Column offset

--- src/index.js
+++ src/index.js
@@ -5,76 +5,27 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
 
-import { Col, Divider, Row } from 'antd';
-const style = {
-  background: '#0092ff',
-  padding: '8px 0',
-};
+import { Col, Row } from 'antd';
 
 const App = () => (
   <>
-    <Divider orientation="left">Horizontal</Divider>
-    <Row gutter={16}>
-      <Col className="gutter-row" span={6}>
-        <div style={style}>col-6-11</div>
-      </Col>
-      <Col className="gutter-row" span={6}>
-        <div style={style}>col-6-12</div>
-      </Col>
-      <Col className="gutter-row" span={6}>
-        <div style={style}>col-6-13</div>
-      </Col>
-      <Col className="gutter-row" span={6}>
-        <div style={style}>col-6-14</div>
+    <Row>
+      <Col span={8}>col-8</Col>
+      <Col span={8} offset={8}>
+        col-8
       </Col>
     </Row>
-    <Divider orientation="left">Responsive</Divider>
-    <Row
-      gutter={{
-        xs: 8,
-        sm: 16,
-        md: 24,
-        lg: 32,
-      }}
-    >
-      <Col className="gutter-row" span={6}>
-        <div style={style}>col-6</div>
+    <Row>
+      <Col span={6} offset={6}>
+        col-6 col-offset-6
       </Col>
-      <Col className="gutter-row" span={6}>
-        <div style={style}>col-6</div>
-      </Col>
-      <Col className="gutter-row" span={6}>
-        <div style={style}>col-6</div>
-      </Col>
-      <Col className="gutter-row" span={6}>
-        <div style={style}>col-6</div>
+      <Col span={6} offset={6}>
+        col-6 col-offset-6
       </Col>
     </Row>
-    <Divider orientation="left">Vertical</Divider>
-    <Row gutter={[16, 24]}>
-      <Col className="gutter-row" span={6}>
-        <div style={style}>col-6</div>
-      </Col>
-      <Col className="gutter-row" span={6}>
-        <div style={style}>col-6</div>
-      </Col>
-      <Col className="gutter-row" span={6}>
-        <div style={style}>col-6</div>
-      </Col>
-      <Col className="gutter-row" span={6}>
-        <div style={style}>col-6</div>
-      </Col>
-      <Col className="gutter-row" span={6}>
-        <div style={style}>col-6</div>
-      </Col>
-      <Col className="gutter-row" span={6}>
-        <div style={style}>col-6</div>
-      </Col>
-      <Col className="gutter-row" span={6}>
-        <div style={style}>col-6</div>
-      </Col>
-      <Col className="gutter-row" span={6}>
-        <div style={style}>col-6</div>
+    <Row>
+      <Col span={12} offset={6}>
+        col-12 col-offset-6
       </Col>
     </Row>
   </>
