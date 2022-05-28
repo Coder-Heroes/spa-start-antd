d0fe4c77ea8cf37c46b1edb951a3a7ebaeccb635
RFNoC <rj.geng@gmail.com>
2022-05-27 17:41:19 -0700

    Button: Multiple Buttons

--- src/index.js
+++ src/index.js
@@ -5,57 +5,37 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
 
-import { Button } from 'antd';
+import { Button, Dropdown, Menu } from 'antd';
+
+const onMenuClick = e => {
+  console.log('click', e);
+};
+
+const menu = (
+  <Menu
+    onClick={onMenuClick}
+    items={[
+      {
+        key: '1',
+        label: '1st item',
+      },
+      {
+        key: '2',
+        label: '2nd item',
+      },
+      {
+        key: '3',
+        label: '3rd item',
+      },
+    ]}
+  />
+);
 
 const App = () => (
   <>
-    <Button type="primary">Primary</Button>
-    <Button type="primary" disabled>
-      Primary(disabled)
-    </Button>
-    <br />
-    <Button>Default</Button>
-    <Button disabled>Default(disabled)</Button>
-    <br />
-    <Button type="dashed">Dashed</Button>
-    <Button type="dashed" disabled>
-      Dashed(disabled)
-    </Button>
-    <br />
-    <Button type="text">Text</Button>
-    <Button type="text" disabled>
-      Text(disabled)
-    </Button>
-    <br />
-    <Button type="link">Link</Button>
-    <Button type="link" disabled>
-      Link(disabled)
-    </Button>
-    <br />
-    <Button danger>Danger Default</Button>
-    <Button danger disabled>
-      Danger Default(disabled)
-    </Button>
-    <br />
-    <Button danger type="text">
-      Danger Text
-    </Button>
-    <Button danger type="text" disabled>
-      Danger Text(disabled)
-    </Button>
-    <br />
-    <Button type="link" danger>
-      Danger Link
-    </Button>
-    <Button type="link" danger disabled>
-      Danger Link(disabled)
-    </Button>
-    <div className="site-button-ghost-wrapper">
-      <Button ghost>Ghost</Button>
-      <Button ghost disabled>
-        Ghost(disabled)
-      </Button>
-    </div>
+    <Button type="primary">primary</Button>
+    <Button>secondary</Button>
+    <Dropdown.Button overlay={menu}>Actions</Dropdown.Button>
   </>
 );
 
