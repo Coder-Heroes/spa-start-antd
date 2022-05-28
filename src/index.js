71a7bd09b6a01f07ca41303cb2266ace791a4f2b
RFNoC <rj.geng@gmail.com>
2022-05-27 21:03:09 -0700

    Grid: Order

--- src/index.js
+++ src/index.js
@@ -7,57 +7,92 @@ import './index.css';
 
 import { Col, Divider, Row } from 'antd';
 
-const DemoBox = props => (
-  <p className={`height-${props.value}`}>{props.children}</p>
-);
-
 const App = () => (
   <>
-    <Divider orientation="left">Align Top</Divider>
-    <Row justify="center" align="top">
-      <Col span={4}>
-        <DemoBox value={100}>col-4</DemoBox>
+    <Divider orientation="left">Normal</Divider>
+    <Row>
+      <Col span={6} order={4}>
+        1 col-order-4
       </Col>
-      <Col span={4}>
-        <DemoBox value={50}>col-4</DemoBox>
+      <Col span={6} order={3}>
+        2 col-order-3
       </Col>
-      <Col span={4}>
-        <DemoBox value={120}>col-4</DemoBox>
+      <Col span={6} order={2}>
+        3 col-order-2
       </Col>
-      <Col span={4}>
-        <DemoBox value={80}>col-4</DemoBox>
-      </Col>
-    </Row>
-
-    <Divider orientation="left">Align Middle</Divider>
-    <Row justify="space-around" align="middle">
-      <Col span={4}>
-        <DemoBox value={100}>col-4</DemoBox>
-      </Col>
-      <Col span={4}>
-        <DemoBox value={50}>col-4</DemoBox>
-      </Col>
-      <Col span={4}>
-        <DemoBox value={120}>col-4</DemoBox>
-      </Col>
-      <Col span={4}>
-        <DemoBox value={80}>col-4</DemoBox>
+      <Col span={6} order={1}>
+        4 col-order-1
       </Col>
     </Row>
-
-    <Divider orientation="left">Align Bottom</Divider>
-    <Row justify="space-between" align="bottom">
-      <Col span={4}>
-        <DemoBox value={100}>col-4</DemoBox>
+    <Divider orientation="left">Responsive</Divider>
+    <Row>
+      <Col
+        span={6}
+        xs={{
+          order: 1,
+        }}
+        sm={{
+          order: 2,
+        }}
+        md={{
+          order: 3,
+        }}
+        lg={{
+          order: 4,
+        }}
+      >
+        1 col-order-responsive
       </Col>
-      <Col span={4}>
-        <DemoBox value={50}>col-4</DemoBox>
+      <Col
+        span={6}
+        xs={{
+          order: 2,
+        }}
+        sm={{
+          order: 1,
+        }}
+        md={{
+          order: 4,
+        }}
+        lg={{
+          order: 3,
+        }}
+      >
+        2 col-order-responsive
       </Col>
-      <Col span={4}>
-        <DemoBox value={120}>col-4</DemoBox>
+      <Col
+        span={6}
+        xs={{
+          order: 3,
+        }}
+        sm={{
+          order: 4,
+        }}
+        md={{
+          order: 2,
+        }}
+        lg={{
+          order: 1,
+        }}
+      >
+        3 col-order-responsive
       </Col>
-      <Col span={4}>
-        <DemoBox value={80}>col-4</DemoBox>
+      <Col
+        span={6}
+        xs={{
+          order: 4,
+        }}
+        sm={{
+          order: 3,
+        }}
+        md={{
+          order: 1,
+        }}
+        lg={{
+          order: 2,
+        }}
+      >
+        4 col-order-responsive
       </Col>
     </Row>
   </>
