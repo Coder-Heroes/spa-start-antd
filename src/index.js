286f4dc6717c05da92c39fde4354c40f8d1e960e
RFNoC <rj.geng@gmail.com>
2022-05-27 17:07:03 -0700

    Button: Size

--- src/index.js
+++ src/index.js
@@ -5,17 +5,59 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
 
-import { Button } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
+import { Button, Radio } from 'antd';
+import { useState } from 'react';
 
-const App = () => (
-  <>
-    <Button type="primary">Primary Button</Button>
-    <Button>Default Button</Button>
-    <Button type="dashed">Dashed Button</Button>
-    <br />
-    <Button type="text">Text Button</Button>
-    <Button type="link">Link Button</Button>
-  </>
-);
+const App = () => {
+  const [size, setSize] = useState('large');
+  return (
+    <>
+      <Radio.Group value={size} onChange={e => setSize(e.target.value)}>
+        <Radio.Button value="large">Large</Radio.Button>
+        <Radio.Button value="default">Default</Radio.Button>
+        <Radio.Button value="small">Small</Radio.Button>
+      </Radio.Group>
+      <br />
+      <br />
+      <Button type="primary" size={size}>
+        Primary
+      </Button>
+      <Button size={size}>Default</Button>
+      <Button type="dashed" size={size}>
+        Dashed
+      </Button>
+      <br />
+      <Button type="link" size={size}>
+        Link
+      </Button>
+      <br />
+      <Button type="primary" icon={<DownloadOutlined />} size={size} />
+      <Button
+        type="primary"
+        shape="circle"
+        icon={<DownloadOutlined />}
+        size={size}
+      />
+      <Button
+        type="primary"
+        shape="round"
+        icon={<DownloadOutlined />}
+        size={size}
+      />
+      <Button
+        type="primary"
+        shape="round"
+        icon={<DownloadOutlined />}
+        size={size}
+      >
+        Download
+      </Button>
+      <Button type="primary" icon={<DownloadOutlined />} size={size}>
+        Download
+      </Button>
+    </>
+  );
+};
 
 render(<App />, document.getElementById('root'));
