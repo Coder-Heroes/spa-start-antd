9ec86a78a025ab2ec6e65be6e0d8fd5019dc883e
RFNoC <rj.geng@gmail.com>
2022-05-27 17:24:53 -0700

    Button: Icon

--- src/index.js
+++ src/index.js
@@ -5,20 +5,81 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
 
-import { Button } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import { Button, Tooltip } from 'antd';
 
 const App = () => (
   <>
-    <Button type="primary" block>
-      Primary
+    <Tooltip title="search">
+      <Button type="primary" shape="circle" icon={<SearchOutlined />} />
+    </Tooltip>
+    <Button type="primary" shape="circle">
+      A
     </Button>
-    <Button block>Default</Button>
-    <Button type="dashed" block>
-      Dashed
+    <Button type="primary" icon={<SearchOutlined />}>
+      Search
     </Button>
-    <Button type="link" block>
-      Link
+    <Tooltip title="search">
+      <Button shape="circle" icon={<SearchOutlined />} />
+    </Tooltip>
+    <Button icon={<SearchOutlined />}>Search</Button>
+    <br />
+    <Tooltip title="search">
+      <Button shape="circle" icon={<SearchOutlined />} />
+    </Tooltip>
+    <Button icon={<SearchOutlined />}>Search</Button>
+    <Tooltip title="search">
+      <Button type="dashed" shape="circle" icon={<SearchOutlined />} />
+    </Tooltip>
+    <Button type="dashed" icon={<SearchOutlined />}>
+      Search
     </Button>
+    <Button icon={<SearchOutlined />} href="https://www.google.com" />
+    <br />
+    <br />
+    <Tooltip title="search">
+      <Button
+        type="primary"
+        shape="circle"
+        icon={<SearchOutlined />}
+        size="large"
+      />
+    </Tooltip>
+    <Button type="primary" shape="circle" size="large">
+      A
+    </Button>
+    <Button type="primary" icon={<SearchOutlined />} size="large">
+      Search
+    </Button>
+    <Tooltip title="search">
+      <Button shape="circle" icon={<SearchOutlined />} size="large" />
+    </Tooltip>
+    <Button icon={<SearchOutlined />} size="large">
+      Search
+    </Button>
+    <br />
+    <Tooltip title="search">
+      <Button shape="circle" icon={<SearchOutlined />} size="large" />
+    </Tooltip>
+    <Button icon={<SearchOutlined />} size="large">
+      Search
+    </Button>
+    <Tooltip title="search">
+      <Button
+        type="dashed"
+        shape="circle"
+        icon={<SearchOutlined />}
+        size="large"
+      />
+    </Tooltip>
+    <Button type="dashed" icon={<SearchOutlined />} size="large">
+      Search
+    </Button>
+    <Button
+      icon={<SearchOutlined />}
+      size="large"
+      href="https://www.google.com"
+    />
   </>
 );
 
