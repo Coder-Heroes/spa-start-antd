4f5fc288dc2a8476ef1638dfc11486d37133fae3
RFNoC <rj.geng@gmail.com>
2022-05-27 17:20:04 -0700

    Button: Ghost Button

--- src/index.js
+++ src/index.js
@@ -5,73 +5,21 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
 
-import { PoweroffOutlined } from '@ant-design/icons';
-import { Button, Space } from 'antd';
-import { useState } from 'react';
+import { Button } from 'antd';
 
-const App = () => {
-  const [loadings, setLoadings] = useState([]);
-
-  const enterLoading = index => {
-    setLoadings(prevLoadings => {
-      const newLoadings = [...prevLoadings];
-      newLoadings[index] = true;
-      return newLoadings;
-    });
-    setTimeout(() => {
-      setLoadings(prevLoadings => {
-        const newLoadings = [...prevLoadings];
-        newLoadings[index] = false;
-        return newLoadings;
-      });
-    }, 6000);
-  };
-
-  return (
-    <>
-      <Space
-        style={{
-          width: '100%',
-        }}
-      >
-        <Button type="primary" loading>
-          Loading
-        </Button>
-        <Button type="primary" size="small" loading>
-          Loading
-        </Button>
-        <Button type="primary" icon={<PoweroffOutlined />} loading />
-      </Space>
-
-      <Space
-        style={{
-          width: '100%',
-        }}
-      >
-        <Button
-          type="primary"
-          loading={loadings[0]}
-          onClick={() => enterLoading(0)}
-        >
-          Click me!
-        </Button>
-        <Button
-          type="primary"
-          icon={<PoweroffOutlined />}
-          loading={loadings[1]}
-          onClick={() => enterLoading(1)}
-        >
-          Click me!
-        </Button>
-        <Button
-          type="primary"
-          icon={<PoweroffOutlined />}
-          loading={loadings[2]}
-          onClick={() => enterLoading(2)}
-        />
-      </Space>
-    </>
-  );
-};
+const App = () => (
+  <div className="site-button-ghost-wrapper">
+    <Button type="primary" ghost>
+      Primary
+    </Button>
+    <Button ghost>Default</Button>
+    <Button type="dashed" ghost>
+      Dashed
+    </Button>
+    <Button type="primary" danger ghost>
+      Danger
+    </Button>
+  </div>
+);
 
 render(<App />, document.getElementById('root'));
