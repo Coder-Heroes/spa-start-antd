1775a2376eb4b9315189718c6fe6ffb55336be33
RFNoC <rj.geng@gmail.com>
2022-05-27 19:43:05 -0700

    Space: Align

--- src/index.js
+++ src/index.js
@@ -5,28 +5,39 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
 
-import { Button, Radio, Space } from 'antd';
-import { useState } from 'react';
+import { Button, Space } from 'antd';
 
-const App = () => {
-  const [size, setSize] = useState('small');
-  return (
-    <>
-      <Radio.Group value={size} onChange={e => setSize(e.target.value)}>
-        <Radio value="small">Small</Radio>
-        <Radio value="middle">Middle</Radio>
-        <Radio value="large">Large</Radio>
-      </Radio.Group>
-      <br />
-      <br />
-      <Space size={size}>
+const App = () => (
+  <div className="space-align-container">
+    <div className="space-align-block">
+      <Space align="center">
+        center
         <Button type="primary">Primary</Button>
-        <Button>Default</Button>
-        <Button type="dashed">Dashed</Button>
-        <Button type="link">Link</Button>
+        <span className="mock-block">Block</span>
       </Space>
-    </>
-  );
-};
+    </div>
+    <div className="space-align-block">
+      <Space align="start">
+        start
+        <Button type="primary">Primary</Button>
+        <span className="mock-block">Block</span>
+      </Space>
+    </div>
+    <div className="space-align-block">
+      <Space align="end">
+        end
+        <Button type="primary">Primary</Button>
+        <span className="mock-block">Block</span>
+      </Space>
+    </div>
+    <div className="space-align-block">
+      <Space align="baseline">
+        baseline
+        <Button type="primary">Primary</Button>
+        <span className="mock-block">Block</span>
+      </Space>
+    </div>
+  </div>
+);
 
 render(<App />, document.getElementById('root'));
