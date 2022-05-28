aff06032a66a02ffedf569eafd9b76e4284d0f83
RFNoC <rj.geng@gmail.com>
2022-05-27 20:56:38 -0700

    Grid: Typesetting

--- src/index.js
+++ src/index.js
@@ -5,17 +5,58 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
 
-import { Col, Row } from 'antd';
+import { Col, Divider, Row } from 'antd';
 
 const App = () => (
-  <Row>
-    <Col span={18} push={6}>
-      col-18 col-push-6
-    </Col>
-    <Col span={6} pull={18}>
-      col-6 col-pull-18
-    </Col>
-  </Row>
+  <>
+    <Divider orientation="left">sub-element align left</Divider>
+    <Row justify="start">
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+    </Row>
+
+    <Divider orientation="left">sub-element align center</Divider>
+    <Row justify="center">
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+    </Row>
+
+    <Divider orientation="left">sub-element align right</Divider>
+    <Row justify="end">
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+    </Row>
+
+    <Divider orientation="left">sub-element monospaced arrangement</Divider>
+    <Row justify="space-between">
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+    </Row>
+
+    <Divider orientation="left">sub-element align full</Divider>
+    <Row justify="space-around">
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+    </Row>
+
+    <Divider orientation="left">sub-element align evenly</Divider>
+    <Row justify="space-evenly">
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+      <Col span={4}>col-4</Col>
+    </Row>
+  </>
 );
 
 render(<App />, document.getElementById('root'));
