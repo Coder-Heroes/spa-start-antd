4b4b335c81cf261851a45923e9d3c8f131b5c0e5
RFNoC <rj.geng@gmail.com>
2022-05-27 20:18:40 -0700

    Space: Split

--- src/index.js
+++ src/index.js
@@ -5,16 +5,13 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
 
-import { Button, Space } from 'antd';
+import { Divider, Space, Typography } from 'antd';
 
 const App = () => (
-  <Space size={[8, 16]} wrap>
-    {new Array(20).fill(null).map((
-      _,
-      index // eslint-disable-next-line react/no-array-index-key
-    ) => (
-      <Button key={index}>Button</Button>
-    ))}
+  <Space split={<Divider type="vertical" />}>
+    <Typography.Link>Link</Typography.Link>
+    <Typography.Link>Link</Typography.Link>
+    <Typography.Link>Link</Typography.Link>
   </Space>
 );
 
